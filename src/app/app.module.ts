d05b1e6ebf2175b54350687b79c6b6ec84d2e917
lucianobritodev <lucianobrito.dev@gmail.com>
2023-01-08 10:17:11 -0300

    clear project and added calendar lib

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { CalendarModule } from '@syncfusion/ej2-angular-calendars'
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CalendarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
